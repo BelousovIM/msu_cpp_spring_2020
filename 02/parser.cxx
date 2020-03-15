21cf36735fd167712913219d588024484f009b4d
Belousov Illarion <50050817+BelousovIM@users.noreply.github.com>
2020-03-15 20:45:54 +0300

    Add files via upload

---
+++ 02/parser.cxx
@@ -0,0 +1,37 @@
+#include "functions.h"
+
+void parser(const std::string s, onPart onStart, onPart onEnd,
+			onItem onNumber, onItem onText
+			)
+{
+	onStart();
+	std::stringstream ss(s);
+	std::string str;
+	while (ss >> str)
+	{
+		if (digit(str))
+			onNumber(str);
+		else if (alpha(str))
+			onText(str);
+	}
+	onEnd();
+}
+bool digit(const std::string s)
+{
+	int i = 0;
+	while(s[i])
+		if (!isdigit(s[i++]))
+			return false;
+
+	return true;
+}
+bool alpha(const std::string s)
+{
+	int i = 0;
+	while(s[i])
+		if (!isalpha(s[i++]))
+			return false;
+
+	return true;
+}
+
