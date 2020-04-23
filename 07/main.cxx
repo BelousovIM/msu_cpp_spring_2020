d1cc98a8f2999492a74be9774a287d62a9de9cdd
Belousov Illarion <50050817+BelousovIM@users.noreply.github.com>
2020-04-23 12:32:22 +0300

    Add files via upload

--- 07/main.cxx
+++ 07/main.cxx
@@ -16,29 +16,29 @@ void assert_equals(const Vector<T>& myVec, const std::vector<T> Vec)
 
 int main()
 {
-    Vector<int> myVec = {100, 100, 700};
-    std::vector<int> Vec = {100, 100, 700};
-    assert_equals(myVec, Vec);
-    
-	
+	Vector<int> myVec = {100, 100, 700};
+	std::vector<int> Vec = {100, 100, 700};
+	assert_equals(myVec, Vec);
+
+
 	myVec.pop_back();
-    myVec.push_back(100);
-    Vec.pop_back();
-    Vec.push_back(100);
-    assert_equals(myVec, Vec);
+	myVec.push_back(100);
+	Vec.pop_back();
+	Vec.push_back(100);
+	assert_equals(myVec, Vec);
 
 	for (auto i = myVec.begin(); i != myVec.end(); ++i) 
 	{
 		assert(*i == 100);
 	}
-	
+
 	for (auto i = myVec.rbegin(); i != myVec.rend(); ++i) 
 	{
 		assert(*i == 100);
 	}
 	
-    myVec.clear(); 
-    Vec.clear();
-    assert(myVec.size() == Vec.size());
-    return 0;
+	myVec.clear(); 
+	Vec.clear();
+	assert(myVec.size() == Vec.size());
+	return 0;
 }
